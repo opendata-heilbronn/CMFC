134777fcf3d1fcbf076cc4bf2f253b39bc0a9cf8
LeoDJ <leandro.spaeth@gmail.com>
2018-07-10 22:12:46 +0200

    add led module

---
+++ makerFactoryEvalBoard/test1/src/led.h
@@ -0,0 +1,30 @@
+#include <NeoPixelBus.h>
+#define LED
+
+#define NUM_LEDS 4
+#define DATA_PIN 33
+
+#define colorSaturation 16
+
+NeoPixelBus<NeoGrbFeature, Neo800KbpsMethod> strip(NUM_LEDS, DATA_PIN);
+
+RgbColor red(colorSaturation, 0, 0);
+RgbColor green(0, colorSaturation, 0);
+RgbColor blue(0, 0, colorSaturation);
+RgbColor white(colorSaturation);
+RgbColor black(0);
+
+
+
+void ledSetup() {
+    strip.Begin();
+    strip.Show();
+}
+
+void ledLoop() {
+    strip.SetPixelColor(0, red);
+    strip.SetPixelColor(1, green);
+    strip.SetPixelColor(2, blue);
+    strip.SetPixelColor(3, white);
+    strip.Show();
+}
--- makerFactoryEvalBoard/test1/src/src.ino
+++ makerFactoryEvalBoard/test1/src/src.ino
@@ -1,9 +1,16 @@
 #include <Arduino.h>
+#include "led.h"
 
 void setup() {
-    
+    Serial.begin(115200);
+
+    #ifdef LED
+    ledSetup();
+    #endif
 }
 
 void loop() {
-    
+    #ifdef LED
+    ledLoop();
+    #endif
 }
